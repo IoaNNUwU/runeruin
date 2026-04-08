4f18ce8cf9455da5ee39c839fa56d5e9266bad6f
IoaNNUwU <ioannxd@gmail.com>
2026-04-09 01:33:22 +0200

    Customize biome

--- src/main/java/ioann/uwu/runeruin/dimension/RRBiomes.java
+++ src/main/java/ioann/uwu/runeruin/dimension/RRBiomes.java
@@ -1,11 +1,20 @@
 package ioann.uwu.runeruin.dimension;
 
 import ioann.uwu.runeruin.RR;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BiomeDefaultFeatures;
 import net.minecraft.data.worldgen.BootstrapContext;
-import net.minecraft.data.worldgen.biome.OverworldBiomes;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.attribute.EnvironmentAttributes;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.biome.BiomeSpecialEffects;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class RRBiomes {
 
@@ -13,12 +22,50 @@ public class RRBiomes {
 
     public static void bootstrap(BootstrapContext<Biome> ctx) {
 
-        var biome = OverworldBiomes.meadowOrCherryGrove(
+        var biome = eldenGarden(
                 ctx.lookup(Registries.PLACED_FEATURE),
-                ctx.lookup(Registries.CONFIGURED_CARVER),
-                true
+                ctx.lookup(Registries.CONFIGURED_CARVER)
         );
 
         ctx.register(ELDEN_GARDEN, biome);
+    }
+
+    public static Biome eldenGarden(HolderGetter<PlacedFeature> placedFeatures, HolderGetter<ConfiguredWorldCarver<?>> carvers) {
+        MobSpawnSettings.Builder mobs = new MobSpawnSettings.Builder()
+                .addSpawn(MobCategory.CREATURE, 1, new MobSpawnSettings.SpawnerData(EntityType.PIG, 1, 2))
+                .addSpawn(MobCategory.CREATURE, 2, new MobSpawnSettings.SpawnerData(EntityType.RABBIT, 2, 4))
+                .addSpawn(MobCategory.CREATURE, 2, new MobSpawnSettings.SpawnerData(EntityType.SHEEP, 2, 4));
+
+        BiomeGenerationSettings.Builder generation = new BiomeGenerationSettings.Builder(placedFeatures, carvers);
+
+        BiomeDefaultFeatures.addDefaultCarversAndLakes(generation);
+        BiomeDefaultFeatures.addDefaultCrystalFormations(generation);
+        BiomeDefaultFeatures.addDefaultMonsterRoom(generation);
+        BiomeDefaultFeatures.addDefaultUndergroundVariety(generation);
+        BiomeDefaultFeatures.addDefaultSprings(generation);
+        BiomeDefaultFeatures.addSurfaceFreezing(generation);
+
+        BiomeDefaultFeatures.addPlainGrass(generation);
+        BiomeDefaultFeatures.addDefaultOres(generation);
+        BiomeDefaultFeatures.addDefaultSoftDisks(generation);
+
+        BiomeDefaultFeatures.addCherryGroveVegetation(generation);
+        BiomeDefaultFeatures.addExtraEmeralds(generation);
+
+        BiomeSpecialEffects.Builder effects = new BiomeSpecialEffects.Builder()
+                .waterColor(0xAA9000)
+                .grassColorOverride(0xAA9000);
+
+        Biome.BiomeBuilder biomeBuilder = new Biome.BiomeBuilder()
+                .hasPrecipitation(true)
+                .temperature(10f)
+                .downfall(10f)
+                .setAttribute(EnvironmentAttributes.SKY_COLOR, 0xAA9000)
+                .setAttribute(EnvironmentAttributes.SUNRISE_SUNSET_COLOR, 0xFF0000)
+                .mobSpawnSettings(mobs.build())
+                .generationSettings(generation.build())
+                .specialEffects(effects.build());
+
+        return biomeBuilder.build();
     }
 }
