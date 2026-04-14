df37cc7bd85e8747d5039a63563e9139aafc9af0
IoaNNUwU <ioannxd@gmail.com>
2026-04-14 20:35:02 +0200

    Make mossberries poison when eaten

--- src/main/java/ioann/uwu/runeruin/items/RRItems.java
+++ src/main/java/ioann/uwu/runeruin/items/RRItems.java
@@ -2,10 +2,15 @@ package ioann.uwu.runeruin.items;
 
 import ioann.uwu.runeruin.RR;
 import ioann.uwu.runeruin.blocks.RRBlocks;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
+import net.minecraft.world.item.consume_effects.ConsumeEffect;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -31,6 +36,15 @@ public class RRItems {
             p -> p.food(new FoodProperties.Builder()
                     .nutrition(2)
                     .saturationModifier(0.1f)
-                    .build())
+                    .build(),
+                    Consumable.builder()
+                            .onConsume(new ApplyStatusEffectsConsumeEffect(
+                                    new MobEffectInstance(
+                                            MobEffects.POISON,
+                                            5 * 20,
+                                            0
+                                    )
+                            ))
+                            .build())
     );
 }
