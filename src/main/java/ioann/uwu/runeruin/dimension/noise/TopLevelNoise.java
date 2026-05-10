85b99803f92093fd9f371647ef12979c01082cac
IoaNNUwU <ioannxd@gmail.com>
2026-05-10 21:36:07 +0200

    Fix top layer generation on layer borders

--- src/main/java/ioann/uwu/runeruin/dimension/noise/TopLevelNoise.java
+++ src/main/java/ioann/uwu/runeruin/dimension/noise/TopLevelNoise.java
@@ -5,7 +5,7 @@ public class TopLevelNoise implements Noise {
     private final Noise moreFlattenedBaseTopLevelNoise;
     private final Noise flattenedBaseTopLevelNoise;
 
-    private final Noise miniNoise = Noise.flatten(0.22f, Noise.multi(
+    private final Noise miniNoise1 = Noise.flatten(0.22f, Noise.multi(
             new SingleNoise("miniNoise4".hashCode(), 0.9f),
             new SingleNoise("miniNoise5".hashCode(), 1.4f),
             new SingleNoise("miniNoise11".hashCode(), 1.4f),
@@ -14,7 +14,7 @@ public class TopLevelNoise implements Noise {
 
     public TopLevelNoise(Noise baseTopLevelNoise, Noise flattenedBaseTopLevelNoise) {
 
-        Noise lowerBaseTopLevelNoise = (x, y, z) -> Math.max(baseTopLevelNoise.noise(x, y, z) - 0.04f, 0.0f);
+        Noise lowerBaseTopLevelNoise = (x, y, z) -> Math.max(baseTopLevelNoise.noise(x, y, z) - 0.045f, 0.0f);
         this.moreFlattenedBaseTopLevelNoise = Noise.flatten(0.1f, lowerBaseTopLevelNoise);
 
         this.flattenedBaseTopLevelNoise = flattenedBaseTopLevelNoise;
@@ -24,12 +24,12 @@ public class TopLevelNoise implements Noise {
     public float noise(float x, float y, float z) {
 
         float base = flattenedBaseTopLevelNoise.noise(x, y, z);
-
         float baseWithEdge = moreFlattenedBaseTopLevelNoise.noise(x, y, z);
-        float miniNoise = baseWithEdge * this.miniNoise.noise(x, y, z);
+
+        float miniNoise1 = baseWithEdge * this.miniNoise1.noise(x, y, z);
 
         return (base * 0.45f
-                + miniNoise * 0.5f
+                + miniNoise1 * 0.5f
         );
     }
 }
